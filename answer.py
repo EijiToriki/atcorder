00c6d9fa0ad85d3708ef6e24053720444d3b6dec
Eiji Toriki <ageForEngi@outlook.jp>
2022-08-04 23:42:15 +0900

    DP練習 C まだ途中

--- answer.py
+++ answer.py
@@ -1,18 +1,23 @@
-N, K = map(int, input().split())
-H = list(map(int, input().split()))
+N = int(input())
 
-dp = [float('inf') for _ in range(N)]
-dp[0] = 0
+dp = [0 for _ in range(N)]
 
-for i in range(1,N):
-  dp_ans = float('inf')
-  for j in range(1, K+1):
-    if i-j < 0:
-      break
-    else:
-      if dp_ans > dp[i-j] + abs(H[i]-H[i-j]):
-        dp_ans = dp[i-j] + abs(H[i]-H[i-j])
-    
-  dp[i] = dp_ans
+
+pre_max_index = -1
+for i in range(N):
+  happiness = list(map(int, input().split()))
+  max_val = 0
+  max_index = -1
+
+  for j in range(3):
+    if j != pre_max_index:
+      if max_val < happiness[j]:
+        max_val = happiness[j]
+        max_index = j
+      elif max_val == happiness[j]:
+        max_index = -1
+
+  pre_max_index = max_index
+  dp[i] = dp[i-1] + max_val
 
 print(dp[N-1])
