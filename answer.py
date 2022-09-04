13eb0e003a513fea24a398b607b782885b2daae0
Eiji Toriki <ageForEngi@outlook.jp>
2022-09-04 22:38:58 +0900

    ABC267 C解答

--- answer.py
+++ answer.py
@@ -1,38 +1,23 @@
-# import sys
-# N, M = map(int, input().split())
-# A = list(map(int, input().split()))
-
-# S = [0]*(N+1)
-# for i in range(N):
-#     S[i+1] = S[i] + A[i] * (i+1)
-
-# max_val = -sys.maxsize
-# max_index = 0
-# for i in range(1, N-M+1):
-#     if max_val < S[i+M]-S[i]:
-#         max_index = i
-#         max_val = S[i+M]-S[i]
-
-# ans = 0
-# for i in range(M):
-#     ans += (i+1)*A[max_index+i]
-
-# print(ans)
-
 import sys
 
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 
-max_val = -sys.maxsize
+S = [0]*(N+1)
+for i in range(1, N+1):
+    S[i] = S[i-1] + A[i-1]
+
+sumi = [-float('INF')]*(N-M+1)
+now = 0
+for i in range(M):
+    now += A[i]*(i+1)
+sumi[0] = now
+
+for i in range(1, N-M+1):
+    sumi[i] = sumi[i-1]+M*A[M+i-1]-(S[M+i-1]-S[i-1])
+
+ans = -sys.maxsize
 for i in range(N-M+1):
-    val = 0
-    cnt = 1
-    for j in range(i, i+M):
-        val +=  cnt * A[j]
-        cnt += 1
-        
-    if ans < val:
-        ans = val
+    ans = max(ans, sumi[i])
 
 print(ans)
