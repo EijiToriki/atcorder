fbbc58569febead0fffa1c46cc6c9aef2f4c286a
Eiji Toriki <ageForEngi@outlook.jp>
2022-09-07 21:47:00 +0900

    ABC255 D解答

--- ABC_Answer/D/255.py
+++ ABC_Answer/D/255.py
@@ -1,9 +1,16 @@
+import bisect
+
 N, Q = map(int, input().split())
 A = list(map(int, input().split()))
- 
+
+A = sorted(A)
+S = [0] * (N+1)
+
+for i in range(N):
+    S[i+1] = S[i] + A[i]
+
 for i in range(Q):
     x = int(input())
-    ans = 0
-    for a in A:
-         ans += abs(a-x)
+    mark = bisect.bisect_left(A, x)
+    ans = x*mark - S[mark] + (S[N] - S[mark]) - x*(N-mark)
     print(ans)
