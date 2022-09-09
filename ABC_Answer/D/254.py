0df5e37edc2423b3c5db23907a5e4cfbf3eec9a3
Eiji Toriki <ageForEngi@outlook.jp>
2022-09-09 22:51:44 +0900

    ABC254 D解答

--- ABC_Answer/D/254.py
+++ ABC_Answer/D/254.py
@@ -1,24 +1,37 @@
-import math
+# 参考：https://qiita.com/sano192/items/d8344f9b9a7bae3a1a6e#d---together-squaredif1191
 
-def make_divisors(n, limit):
-    lower_divisors, upper_divisors = [], []
-    i = math.floor(math.sqrt(n))
-    while i > 0:
-        if n % i == 0:
-            if i<= limit and n//i <= limit:
-                lower_divisors.append(i)
-                if i != n//i:
-                    upper_divisors.append(n//i)
-            else:
-                break
-        i -= 1
-    return lower_divisors + upper_divisors[::-1]
+from bisect import bisect_right
+
+def factorization(n):
+    arr = []
+    temp = n
+    for i in range(2, int(-(-n**0.5//1))+1):
+        if temp%i==0:
+            cnt=0
+            while temp%i==0:
+                cnt+=1
+                temp //= i
+            arr.append([i, cnt])
+
+    if temp!=1:
+        arr.append([temp, 1])
+
+    if arr==[]:
+        arr.append([n, 1])
+
+    return arr
 
 N = int(input())
-square_list = [i*i for i in range(1, N+1)]
+S = [i*i for i in range(1, N+1)]
 
 ans = 0
-for n in square_list:
-    ans += len(make_divisors(n, N))
+for i in range(1, N+1):
+    factors = factorization(i)
+    j = 1
+    for factor in factors:
+        if factor[1] % 2 == 1:
+            j *= factor[0]
+    tansaku =  bisect_right(S, N//j)
+    ans += tansaku
 
 print(ans)
