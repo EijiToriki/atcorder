8c64e5844bb84c3852c500f483cb129cfbd52823
Eiji Toriki <ageForEngi@outlook.jp>
2022-09-12 11:41:42 +0900

    ABC268 C解答

--- ABC_Answer/C/268.py
+++ ABC_Answer/C/268.py
@@ -1,25 +1,10 @@
-def chineseTable(P, N):
-  result = 0
-  for i in range(len(P)):
-    if i==P[i] or (i-1)%N == P[i] or (i+1)%N == P[i]:
-      result += 1
-
-  return result
-
-
 N = int(input())
 P = list(map(int, input().split()))
-P = P*3
 
-p0 = [i for i, x in enumerate(P) if x == 0][1]
+op_cnt = [0 for _ in range(N)]
+for i in range(len(P)):
+  op_cnt[(N-P[i]+i-1)%N] += 1
+  op_cnt[(N-P[i]+i)%N] += 1
+  op_cnt[(N-P[i]+i+1)%N] += 1
 
-P1 = P[p0:p0+N]
-P2 = P[p0-1:p0+N-1]
-P3 = P[p0+1:p0+N+1]
-
-ans = 0
-ans = max(ans, chineseTable(P1, N))
-ans = max(ans, chineseTable(P2, N))
-ans = max(ans, chineseTable(P3, N))
-
-print(ans)
+print(max(op_cnt))
