2f5e7fb426fb68b431114960ddc26ac26194149b
Eiji Toriki <ageForEngi@outlook.jp>
2022-10-07 13:38:38 +0900

    ABC242 D解答(理解できていない）

---
+++ ABC_Answer/D/242.py
@@ -0,0 +1,25 @@
+def main():
+    def rec(t, k):
+        if t == 0:
+            return pat.index(S[k])
+        if k == 0:
+            return pat.index(S[0]) + t
+        if k % 2 == 0:
+            return rec(t - 1, k // 2) + 1
+        else:
+            return rec(t - 1, k // 2) + 2
+
+    def solve():
+        t, k = map(int, input().split())
+        k -= 1  # 忘れずに
+        return pat[rec(t, k) % 3]
+
+    S = input()
+    Q = int(input())
+    pat = "ABC"
+    for _ in range(Q):
+        print(solve())
+
+
+if __name__ == '__main__':
+    main()
--- answer.py
+++ answer.py
@@ -1,44 +1,25 @@
-S = input()
-Q = int(input())
+def main():
+    def rec(t, k):
+        if t == 0:
+            return pat.index(S[k])
+        if k == 0:
+            return pat.index(S[0]) + t
+        if k % 2 == 0:
+            return rec(t - 1, k // 2) + 1
+        else:
+            return rec(t - 1, k // 2) + 2
 
-for _ in range(Q):
-    t, k = map(int, input().split())
-    sLen = len(S)
+    def solve():
+        t, k = map(int, input().split())
+        k -= 1  # 忘れずに
+        return pat[rec(t, k) % 3]
 
-    if t == 0:
-        print(S[k-1])
-    else:
-        move_cnt = 0
-        while True:
-            if  sLen > k-1:
-                break
-            else:
-                sLen *= 2
-                move_cnt += 1
-        
-        one_len = 2 ** move_cnt
-        base_index = (k-1) // one_len
-        ans_index = (k-1) % one_len
+    S = input()
+    Q = int(input())
+    pat = "ABC"
+    for _ in range(Q):
+        print(solve())
 
-        ans_index = bin(ans_index)[2:]
 
-        ans = S[base_index]
-        for idx in ans_index:
-            if idx == '0':
-                if ans == 'A':
-                    ans = 'B'
-                elif ans == 'B':
-                    ans = 'C'
-                elif ans == 'C':
-                    ans = 'A'
-            else:
-                if ans == 'A':
-                    ans = 'C'
-                elif ans == 'B':
-                    ans = 'A'
-                elif ans == 'C':
-                    ans = 'B'
-        
-        print(ans)
-    
-
+if __name__ == '__main__':
+    main()
