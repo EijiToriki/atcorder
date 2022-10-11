10c2eac314ec2ab4826c02be6f2104594d673329
Eiji Toriki <ageForEngi@outlook.jp>
2022-10-11 22:38:05 +0900

    ABC241 D解答(3パターンTLE)

--- answer.py
+++ answer.py
@@ -1,18 +1,29 @@
-def main(N, A, X):    
-    A_sum = sum(A)
-    ans = (X // A_sum) * N
-    val = (X // A_sum) * A_sum
-    for a in A:
-        if val > X:
-            break
-        val += a
-        ans += 1
-    
-    print(ans)
+
+
+from bisect import bisect, bisect_left, bisect_right, insort_left
+
+
+def main(Q):  
+    A = []  
+    for _ in range(Q):
+        query = list(map(int, input().split()))
+        if query[0] == 1:
+            insort_left(A, query[1])
+        if query[0] == 2:
+            idx = bisect_right(A, query[1])
+            if idx - query[2] < 0:
+                print(-1)
+            else:
+                print(A[idx-query[2]])
+        if query[0] == 3:
+            idx = bisect_left(A, query[1])
+            if idx + query[2] - 1 >= len(A):
+                print(-1)
+            else:
+                print(A[idx+query[2]-1])
+
 
 
 if __name__ == "__main__":
-    N = int(input())
-    A = list(map(int, input().split()))
-    X = int(input())
-    main(N, A, X)
+    Q = int(input())
+    main(Q)
