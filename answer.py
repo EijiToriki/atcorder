5251c00f66f87c35a842997db6e9236171cdedf1
Eiji Toriki <ageForEngi@outlook.jp>
2022-11-01 22:49:22 +0900

    ABC219 A,B解答，C作成中

--- answer.py
+++ answer.py
@@ -1,16 +1,29 @@
-from functools import lru_cache
-from math import floor
-import sys
-sys.setrecursionlimit(10**6)
+from collections import defaultdict
 
-@lru_cache(maxsize=100000)
-def f(n):
-  if n == 0:
-    return 1
-  else:
-    return f(floor(n/2)) + f(floor(n/3))
-
+X = input()
 N = int(input())
-ans = f(N)
-print(ans)
+S = []
+for _ in range(N):
+  S.append(input())
 
+seq_dict = defaultdict(int)
+for i, x in enumerate(X):
+  seq_dict[x] = i + 1
+
+
+S_ctoi = []
+for s in S:
+  ctoi = 0
+  for i in range(10):
+    try:
+      print(s, s[i], seq_dict[s[i]])
+      ctoi += seq_dict[s[i]] * 10 ** (9-i)
+    except IndexError:
+      pass
+  print(ctoi)
+  S_ctoi.append(ctoi)
+
+S_ctoi = sorted(range(len(S_ctoi)), key=S_ctoi.__getitem__)
+
+for i in S_ctoi:
+  print(S[i])
