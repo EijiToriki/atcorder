eea2591754e398a853c0f836d1e4cca7e597eb07
Eiji Toriki <ageForEngi@outlook.jp>
2022-11-02 08:44:41 +0900

    ABC219 C解答

--- answer.py
+++ answer.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from operator import itemgetter
 
 X = input()
 N = int(input())
@@ -6,24 +7,30 @@ S = []
 for _ in range(N):
   S.append(input())
 
-seq_dict = defaultdict(int)
+ctoi_dict = defaultdict(int)
+itoc_dict = defaultdict(str)
 for i, x in enumerate(X):
-  seq_dict[x] = i + 1
-
+  ctoi_dict[x] = i + 1
+  itoc_dict[i+1] = x
 
 S_ctoi = []
 for s in S:
-  ctoi = 0
+  ctoi = []
   for i in range(10):
     try:
-      print(s, s[i], seq_dict[s[i]])
-      ctoi += seq_dict[s[i]] * 10 ** (9-i)
+      ctoi.append(ctoi_dict[s[i]])
     except IndexError:
-      pass
-  print(ctoi)
+      ctoi.append(0)
   S_ctoi.append(ctoi)
 
-S_ctoi = sorted(range(len(S_ctoi)), key=S_ctoi.__getitem__)
+S_ctoi = sorted(S_ctoi, key=itemgetter(0,1,2,3,4,5,6,7,8,9))
 
-for i in S_ctoi:
-  print(S[i])
+for s in S_ctoi:
+  ans_str = ""
+  for i in range(10):
+    try:
+      ans_str += itoc_dict[s[i]]
+    except IndexError:
+      pass
+
+  print(ans_str)
