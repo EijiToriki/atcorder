951a2e1270ce8ddd268a9ab9d46e30f51448f55c
Eiji Toriki <ageForEngi@outlook.jp>
2022-11-16 18:40:30 +0900

    決め打ち二分探索の練習

--- answer.py
+++ answer.py
@@ -1,19 +1,32 @@
-R, B = map(int, input().split())
-x, y = map(int, input().split())
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+
+def check(x):
+  res = 0
+  now_len = 1
+  change_flag = False
+  for i in range(1, N):
+    if change_flag:
+      change_flag = False
+      continue
+    if A[i] == A[i-1]:
+      now_len += 1
+      if now_len > x:
+        res += 1
+        now_len = 1
+        change_flag = True
+    else:
+      now_len = 1
+  return res <= K
+
 
 left = 0
-right = 10**32
-
+right = N
 while right-left > 1:
   mid = (left + right) // 2
-
-  sum = 0
-  if R-mid >= 0 and B-mid >= 0:
-    sum = (R-mid)//(x-1) + (B-mid)//(y-1)
-  
-  if sum >= mid:
-    left = mid
-  else:
+  if check(mid):
     right = mid
+  else:
+    left = mid
 
-print(left)
+print(right)
