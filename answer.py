3adfc21dfc5f4df79210aec7fecee6da57bb3b48
Eiji Toriki <ageForEngi@outlook.jp>
2022-12-02 22:49:23 +0900

    ABC 220 D回答

--- answer.py
+++ answer.py
@@ -1,20 +1,13 @@
-import sys
-sys.setrecursionlimit(10**6)
-
 N = int(input())
 A = list((map(int, input().split())))
-ans = [0] * 10
-depth = 1
+MOD = 998244353
+dp = [[0] * 10 for _ in range(N)]
 
-def f(a,b,d):
-  if d == N-1:
-    ans[(a+b)%10] += 1
-    ans[(a*b)%10] += 1
-    return ans
-  else:
-    F = (a+b)%10
-    G = (a*b)%10
-    d += 1
-    return f(F, A[d], d), f(G, A[d], d)
+dp[0][A[0]] = 1
+for i in range(1, N):
+  for j in range(10):
+    dp[i][(j+A[i]) % 10] = (dp[i][(j+A[i]) % 10] + dp[i-1][j]) % MOD
+    dp[i][(j*A[i]) % 10] = (dp[i][(j*A[i]) % 10] + dp[i-1][j]) % MOD
 
-f()
+for i in range(10):
+  print(dp[N-1][i])
