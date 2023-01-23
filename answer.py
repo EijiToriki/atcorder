4edc41328b34cb889a428ac87b5fdad952baf5de
Eiji Toriki <ageForEngi@outlook.jp>
2023-01-23 20:57:56 +0900

    ABC286 C問題

--- answer.py
+++ answer.py
@@ -1,3 +1,13 @@
 N, A, B = map(int, input().split())
-S = input()
+S = list(input()) * 2
+
+ans = 10**18
+for i in range(N):
+  cost = A * i
+  for j in range(N//2):
+    if S[i+j] != S[N+i-j-1]:
+      cost += B
+  ans = min(ans, cost)
+
+print(ans)
 
