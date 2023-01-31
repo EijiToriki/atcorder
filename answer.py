45c91f9d72d4b201ff1b99e633d494708a8b23ca
Eiji Toriki <ageForEngi@outlook.jp>
2023-01-31 23:15:09 +0900

    ABC287 D力尽きた

--- answer.py
+++ answer.py
@@ -1,43 +1,16 @@
-import sys
-sys.setrecursionlimit(10**6)
+import re
 
-N, M = map(int, input().split())
-if N == 1 and M == 0:
-  print('Yes')
-  exit()
+S = input()
+T = input()
 
-G = [[] for _ in range(N+1)]
-for _ in range(M):
-    u, v = map(int, input().split())
-    G[u].append(v)
-    G[v].append(u)
+S = S.replace('?', '.')
+T = T.replace('?', '.')
 
-visited = [False] * (N+1)
-visited[0] = True
-def dfs(node):
-    visited[node] = True
-
-    for to in G[node]:
-        if visited[to] == False:
-            dfs(to)
-
-
-sg = []
-for i, g in enumerate(G):
-  if len(g) == 1:
-    sg.append(i)
-  elif len(g) > 2:
+T_size = len(T)
+for i in range(T_size+1):
+  s = S[:i] + S[T_size+i-1:]
+  res = re.match(r'(\b)' + s + r'(\b)', T)
+  if res:
+    print('Yes')
+  else:
     print('No')
-    exit()
-
-if len(sg) != 2:
-  print('No')
-  exit()
-
-
-dfs(sg[0])
-if visited.count(True) == N+1:
-  print('Yes')
-else:
-  print('No')
-
