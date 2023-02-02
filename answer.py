ee9881e999ed504ae38e23388cb99105339a7e27
Eiji Toriki <ageForEngi@outlook.jp>
2023-02-02 22:33:42 +0900

    A鉄則14b

--- answer.py
+++ answer.py
@@ -2,25 +2,34 @@ from bisect import bisect_left
 
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-C = list(map(int, input().split()))
-D = list(map(int, input().split()))
 
-AB = []
-CD = []
-for i in range(N):
-  for j in range(N):
-    AB.append(A[i]+B[j])
-    CD.append(C[i]+D[j])
-AB, CD = sorted(AB), sorted(CD)
+half_N = N // 2
+former = []
+for i in range(2**half_N):
+  val = 0
+  for j in range(half_N):
+    if ((i >> j) & 1):
+      val += A[j]
+  former.append(val)
+former = sorted(former)
 
-for ab in AB:
+latter = []
+for i in range(2**(N-half_N)):
+  val = 0
+  for j in range(N-half_N):
+    if ((i >> j) & 1):
+      val += A[half_N + j]
+  latter.append(val)
+latter = sorted(latter)
+
+for f in former:
+  search_num = K - f
+  search_idx = bisect_left(latter, search_num)
   try:
-    search_num = K - ab
-    search_idx = bisect_left(CD, search_num)
-    if CD[search_idx] == search_num:
+    if latter[search_idx] == search_num:
       print('Yes')
-      exit()
+      exit()  
   except IndexError:
     pass
+
 print('No')
