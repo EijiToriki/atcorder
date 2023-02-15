38313cd32a3005784139890ffe3f04485795d722
Eiji Toriki <ageForEngi@outlook.jp>
2023-02-15 21:21:38 +0900

    鉄則22b

--- answer.py
+++ answer.py
@@ -1,12 +1,15 @@
 N = int(input())
-A = [0] + list(map(int, input().split()))
-B = [0] + list(map(int, input().split()))
+A = [0, 0] + list(map(int, input().split()))
+B = [0, 0, 0] + list(map(int, input().split()))
 
-dp = [-10**18] * (N+1)
+dp = [10**18] * (N+1)
+
 dp[1] = 0
-
 for i in range(1, N):
-    dp[A[i]] = max(dp[A[i]], dp[i] + 100)
-    dp[B[i]] = max(dp[B[i]], dp[i] + 150)
+    try:
+        dp[i+1] = min(dp[i+1], dp[i] + A[i+1])
+        dp[i+2] = min(dp[i+2], dp[i] + B[i+2])
+    except IndexError:
+        pass
 
 print(dp[N])
