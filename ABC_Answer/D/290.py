f71a20607454d904010c6d2ffffded0d6fda1a16
Eiji Toriki <ageForEngi@outlook.jp>
2023-02-20 22:33:34 +0900

    ABC 290 D問題, 鉄則A29

--- ABC_Answer/D/290.py
+++ ABC_Answer/D/290.py
@@ -1,13 +1,8 @@
+from math import gcd
+
 T = int(input())
 
 for _ in range(T):
     N, D, K = map(int, input().split())
-    if D > N:
-        D = D % N
-
-    if N >= 2 * D:
-        ans = D * (K-1) % N + (K-1) // (N // D) % N 
-        print(ans)
-    else:
-        ans = D * (K-1) % N + (K-1) // (N // (N-D)) % N 
-        print(ans)
+    ans = D * (K-1) % N + (K-1) // (N // gcd(N, D))
+    print(ans)
