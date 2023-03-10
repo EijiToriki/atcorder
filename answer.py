5ab311b0bfc70f58849b1c3758538ee291e56996
Eiji Toriki <ageForEngi@outlook.jp>
2023-03-10 23:10:51 +0900

    鉄則45 途中

--- answer.py
+++ answer.py
@@ -1,13 +1,33 @@
-N = int(input())
-A = []
-for _ in range(N):
-    a = list(map(int, input().split()))
-    A.append(a)
-A_row_idx = [i for i in range(N)]
-Q = int(input())
-for _ in range(Q):
-    query = list(map(int, input().split()))
-    if query[0] == 1:
-        A_row_idx[query[1]-1], A_row_idx[query[2]-1] = A_row_idx[query[2]-1], A_row_idx[query[1]-1]
-    else:
-        print(A[A_row_idx[query[1]-1]][query[2]-1])
+N, C = input().split()
+A = input()
+
+red_num = 0
+blue_num = 0
+
+for a in A:
+    if a == 'R':
+        red_num += 1
+    if a == 'B':
+        blue_num += 1
+
+color = ''
+if red_num > blue_num:
+    color = 'R'
+elif red_num == blue_num:
+    color = 'W'
+else:
+    color = 'B'
+
+if color == C:
+    print('Yes')
+else:
+    print('No')
+
+
+
+10  0
+8   1
+6   2
+4   3
+2   4
+0   5
