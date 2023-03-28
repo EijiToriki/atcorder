3006d566bd776f83196ffdac61c64f0e12674386
Eiji Toriki <ageForEngi@outlook.jp>
2023-03-29 08:56:39 +0900

    鉄則58B 完

--- answer.py
+++ answer.py
@@ -1,3 +1,5 @@
+import bisect
+
 class SegTree:
     """
     init(init_val, ide_ele): 配列init_valで初期化 O(N)
@@ -62,45 +64,25 @@ class SegTree:
 N, L, R = map(int, input().split())
 X = list(map(int, input().split()))
 
-dp = [[float('inf') for _ in range(N)] for _ in range(N)]
+dp = [0] * N
 
 #####segfunc#####
-# def segfunc(x, y):
-#     return max(x, y)
+def segfunc(x, y):
+    return min(x, y)
 # #################
 
 # #####ide_ele#####
-# ide_ele = -float('inf')
+ide_ele = float('inf')
 #################
 
-# seg = SegTree(A, segfunc, ide_ele)
+seg = SegTree(dp, segfunc, ide_ele)
 
-dp[0][0] = 0
+seg.update(0, 0)
 for i in range(1, N):
-    if L <= X[i] - X[0] <=R:
-        dp[0][i] = 1
+    x = X[i]
+    posL = bisect.bisect_left(X, x-R)
+    posR = bisect.bisect_right(X, x-L)
+    dp[i] = seg.query(posL, posR) + 1
+    seg.update(i, dp[i])
 
-
-for i in range(1, N):
-    for j in range(i+1):
-        dp[i][j] = dp[i-1][j]
-    for j in range(i+1, N):
-        if L <= X[j] - X[i] <= R:
-            dp[i][j] = min(dp[i][j], dp[i][i]+1)
-        else:
-            dp[i][j] = min(dp[i][j], dp[i-1][j])
-
-ans = -1
-for i in range(N):
-    if dp[i][N-1] > ans and dp[i][N-1] != float('inf'):
-        ans = dp[i][N-1]
-
-print(ans)
-
-
-# for _ in range(Q):
-#     query = list(map(int, input().split()))
-#     if query[0] == 1:
-#         seg.update(query[1]-1, query[2])
-#     elif query[0] == 2:
-#         print(seg.query(query[1]-1, query[2]-1))
+print(dp[-1])
