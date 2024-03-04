c812fed51084aef27261dbd74b52eae2416336f2
Eiji Toriki <ageForEngi@outlook.jp>
2024-03-04 22:40:54 +0900

    ABC313(思考中）

--- answer.py
+++ answer.py
@@ -1,60 +1,27 @@
-def get_Tak_Code(N, M, grid):
-  tak_code_region = []
-  for i in range(N-8):
-    for j in range(M-8):
-      if judge_Tak_Code(i, j, grid):
-        tak_code_region.append((i+1, j+1))
-
-  return tak_code_region
-
-def judge_Tak_Code(i, j, grid):
-  if judge_must_black(i, j, grid) and judge_must_white(i, j, grid):
-    return True
-  else:
-    return False
-
-def judge_must_black(i, j, grid):
-  for x in range(3):
-    for y in range(3):
-      if grid[i+x][j+y] != '#':
-        return False
-
-  for x in range(3):
-    for y in range(3):
-      if grid[i+6+x][j+6+y] != '#':
-        return False
-
-  return True
-
-def judge_must_white(i, j, grid):
-  for left_y in range(4):
-    if grid[i+3][j+left_y] != '.':
-      return False
-  
-  for left_x in range(4):
-    if grid[i+left_x][j+3] != '.':
-      return False
-  
-  for right_y in range(4):
-    if grid[i+5][j+5+right_y] != '.':
-      return False
-  
-  for right_x in range(4):
-    if grid[i+5+right_x][j+5] != '.':
-      return False
-  
-  return True
-
-
-def print_ans(anses):
-  for ans in anses:
-    print(*ans)
+from collections import deque
 
 N, M = map(int, input().split())
-S = []
-for _ in range(N):
-  S.append(input())
+A, B = [], []
+for _ in range(M):
+  a, b = map(int, input().split())
+  A.append(a)
+  B.append(b)
 
-anses = get_Tak_Code(N, M, S)
-anses = sorted(anses, key=lambda x:(x[0], x[1]))
-print_ans(anses)
+stronger = []
+for h in range(1, N+1):
+  weaker = deque([h])
+  weak_set = set((h,))
+  while len(weaker) != 0:
+    person = weaker.popleft()
+    for i in range(M):
+      if person == A[i]:
+        weaker.append(B[i])
+        weak_set.add(B[i])
+    if len(weak_set) == N:
+      stronger.append(h)
+      break
+
+if len(stronger) == 1:
+  print(stronger[0])
+else:
+  print(-1)
