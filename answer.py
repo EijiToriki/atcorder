a5973df71ad1c86bac4232cb76f32271c0409dcf
Eiji Toriki <ageForEngi@outlook.jp>
2024-03-05 21:30:34 +0900

    ABC313 B問題

--- answer.py
+++ answer.py
@@ -8,20 +8,17 @@ for _ in range(M):
   B.append(b)
 
 stronger = []
+weak_set = set()
 for h in range(1, N+1):
   weaker = deque([h])
-  weak_set = set((h,))
   while len(weaker) != 0:
     person = weaker.popleft()
     for i in range(M):
-      if person == A[i]:
+      if person == A[i] and B[i] not in weak_set:
         weaker.append(B[i])
         weak_set.add(B[i])
-    if len(weak_set) == N:
-      stronger.append(h)
-      break
+    if len(weak_set) == N-1:
+      print(h)
+      exit()
 
-if len(stronger) == 1:
-  print(stronger[0])
-else:
-  print(-1)
+print(-1)
