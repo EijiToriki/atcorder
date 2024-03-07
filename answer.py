3cc4eddc1f896bb201b54989e4fefbc42e096cf4
Eiji Toriki <ageForEngi@outlook.jp>
2024-03-07 22:30:16 +0900

    ABC312 C問題

--- answer.py
+++ answer.py
@@ -1,27 +1,22 @@
-from bisect import bisect_right
+from bisect import bisect_left, bisect_right
+
+def judge(mid):
+  seller = bisect_right(A, mid)
+  buyer = M - bisect_left(B, mid)
+  return seller >= buyer
+
 
 N, M = map(int, input().split())
 A = sorted(list(map(int, input().split())))
 B = sorted(list(map(int, input().split())))
 
-seller_num = len(A)
-buyer_num = len(B)
+ng = 0
+ok = 10**9 + 1
+while abs(ok -ng) > 1:
+  mid = (ok + ng) // 2
+  if judge(mid):
+    ok = mid
+  else:
+    ng = mid
 
-for sell_idx, a in enumerate(A):
-  if sell_idx + 1 < seller_num and A[sell_idx] != A[sell_idx+1]:
-    can_buy_num = buyer_num - bisect_right(B, a)
-    can_sell_num = sell_idx + 1
-    if can_sell_num >= can_buy_num:
-      print(a)
-      exit()
-  elif sell_idx + 1 == seller_num:
-    can_buy_num = buyer_num - bisect_right(B, a)
-    can_sell_num = sell_idx + 1
-    if can_sell_num >= can_buy_num:
-      if A[seller_num-1] > B[buyer_num-1]:
-        print(B[buyer_num-1]+1)
-      else:
-        print(A[seller_num-1])
-      exit()
-
-
+print(ok)
