813015a8d495c5466aebc728adbafed03cab9d7c
Eiji Toriki <ageForEngi@outlook.jp>
2024-03-09 21:29:30 +0900

    ABC313 C問題 歯が立たず。。

--- ABC_Answer/C/312.py
+++ ABC_Answer/C/312.py
@@ -1,9 +1,22 @@
-from bisect import bisect_right
+from bisect import bisect_left, bisect_right
+
+def judge(mid):
+  seller = bisect_right(A, mid)
+  buyer = M - bisect_left(B, mid)
+  return seller >= buyer
+
 
 N, M = map(int, input().split())
 A = sorted(list(map(int, input().split())))
 B = sorted(list(map(int, input().split())))
 
-for a in A:
-  print(a, bisect_right(B, a))
+ng = 0
+ok = 10**9 + 1
+while abs(ok -ng) > 1:
+  mid = (ok + ng) // 2
+  if judge(mid):
+    ok = mid
+  else:
+    ng = mid
 
+print(ok)
